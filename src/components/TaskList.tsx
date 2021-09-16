dc079540b20ab66bccf06a1b6016c664cf44407d
fjubert <felipe.ajubert@gmail.com>
2021-09-16 00:45:11 -0300

    Desafio Finaliado

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,35 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (newTaskTitle) {
+      let randomId =  (Math.random() * (100000 - 1)) + 1;
+      const task = {
+        id: randomId,
+        title: newTaskTitle,
+        isComplete: false
+      }
+      setTasks([
+        ...tasks,
+        task
+      ]);
+      setNewTaskTitle('');
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const checkTasks = tasks.map( task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } : task);
+
+    setTasks(checkTasks);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const removeTask = tasks.filter( task => task.id !== id);
+    setTasks( removeTask );
   }
 
   return (
